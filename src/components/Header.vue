9fa92491be0aeddc55c1efb27b78d6d4b464c0dc
Ben Godfrey <bcgodfrey91@gmail.com>
2017-08-02 18:16:52 -0500

    add header component, update router

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="header">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      msg: 'This is the header.',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#header {
+  height: 10rem;
+  border: 1px solid black;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="hello">
+    <Header></Header>
     <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header';
+
 export default {
+  components: {
+    Header,
+  },
   name: 'hello',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Butts. Lots of Butts.',
     };
   },
 };
@@ -21,17 +27,4 @@ h1, h2 {
   font-weight: normal;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
